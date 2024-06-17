8eb39cbb5bcce3ce1da931010e8c84f6cd34bd13
VenkataSindhujaM <sindhu.mareedu2@gmail.com>
2024-06-17 13:26:41 +0530

    Modified code

--- Hello1.java
+++ Hello1.java
@@ -1,8 +1,12 @@
+import java.util.*;
 public class Hello1 {
     public static void main(String args[]){
-        for(int i=0;i<5;i++){
-            System.out.println(".*.");  
-              }
+        Scanner sc=new Scanner(System.in);
+        System.out.print("n:");
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++){
+            System.out.println(5*i);
+        }
     }
     
 }
